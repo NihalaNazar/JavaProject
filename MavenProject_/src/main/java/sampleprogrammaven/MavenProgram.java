7a8fa408ee8741d0d755810b2ced0082c33760ea
nihal <nihal@Nihala>
2024-03-27 20:33:58 +0530

    sample maven program

---
+++ MavenProject_/src/main/java/sampleprogrammaven/MavenProgram.java
@@ -0,0 +1,14 @@
+package sampleprogrammaven;
+
+public class MavenProgram 
+{
+	public void print()
+	{
+		System.out.println("Hello world");
+	}
+public static void main(String []args)
+{
+	MavenProgram a=new MavenProgram();
+	a.print();
+}
+}

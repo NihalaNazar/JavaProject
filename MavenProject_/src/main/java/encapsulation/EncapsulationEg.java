e91cab6af4a748741a87206f1383effb61f8587a
nihal <nihal@Nihala>
2024-05-07 19:25:04 +0530

    encapsulation

---
+++ MavenProject_/src/main/java/encapsulation/EncapsulationEg.java
@@ -0,0 +1,18 @@
+package encapsulation;
+
+public class EncapsulationEg
+{
+	private String name;
+	private int id;
+	public void setter(String name,int id)
+	{
+		this.name=name;
+		this.id=id;
+	}
+	public void getter()
+	{
+		System.out.println("Anju");
+		System.out.println(id);
+	}
+
+}
